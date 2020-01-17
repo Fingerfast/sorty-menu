f28cb81b8bffb81e5d39335f0658389cb5e6f26e
Karel Janík <karel.janik@cleverlance.com>
2020-01-17 17:00:14 +0100

    CODE: 'Refactor to hooks, update components wihout Redux'

--- plugins/menu-editor/admin/src/components/MenuEditor/SortableMenu.js
+++ plugins/menu-editor/admin/src/components/MenuEditor/SortableMenu.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import Sortly, { remove, add } from 'react-sortly';
+import React, {useState, useEffect, useCallback} from 'react';
+import Sortly, {remove, add, convert} from 'react-sortly';
 import nanoid from 'nanoid/non-secure';
 import { FormattedMessage } from 'react-intl';
 import SortableTreeItem from './SortableTreeItem';
@@ -10,46 +10,53 @@ const exampleMenu = [
   {"id":"2","order":1,"name":"Pobočky","parent_id":"1","menu_id":"1"},
   {"id":"3","order":0,"name":"Kudy k nám","parent_id":"1","menu_id":"1"}
 ];
+const remapSortlyInput = databaseOutput => {
+  return databaseOutput.map(row => {
+    const {
+      id,
+      parent_id = null,
+      menu_id,
+      order,
+      title,
+    } = row;
+
+    return {
+      id,
+      index: order,
+      name: title,
+    };
+  });
+};
+
+function convertData(data) {
+  const sortlyData = convert(remapSortlyInput(data));
+  return sortlyData;
+}
 
 export default function SortableMenu ({ onChange, menuId }) {
-  // const [{ data: menuData, loading: menuDataLoading, error: menuDataError }] = useAxios(
-  //   //TODO: Put ID to fetch details about this menu
-  //   'https://api.myjson.com/bins/820fc'
-  // )
+  const [{ data: menuData, loading: menuDataLoading, error: menuDataError }, setMenuData] = useAxios(
+    //TODO: Put ID to fetch details about this menu
+    `http://localhost:1337/menu-editor/menu/${menuId}`
+  )
+  const [sortlyData, setSortlyData] = useState([])
+  console.log('MENU ID' , menuId)
+  console.log('MENU DATA' , menuData)
 
-  const remapSortlyInput = databaseOutput => {
-    return databaseOutput.map(row => {
-      const {
-        id,
-        parent_id = null,
-        menu_id,
-        order,
-        title,
-      } = row;
-
-      return {
-        id,
-        index: order,
-        name: title,
-        parentId: parent_id,
-        menu_id,
-      };
-    });
-  };
-
-  const [menuData, updateMenuData] = useState(exampleMenu)
-
-
+  useEffect(() => {
+    menuData && setSortlyData(convert(remapSortlyInput(menuData)))
+  }, [menuDataLoading])
+  console.log('sortlyData' , sortlyData)
 
   const handleChangeRow = (id, target) => {
     const index = menuData.findIndex(item => item.id === id);
-    const { name, value } = target;
-    onChange(
-      'menuData',
-      update(menuData, {
-        [index]: { [name]: { $set: value } },
-      })
-    );
+    console.log('index' , id.target.value, target, index)
+    // const { name, value } = target;
+    // onChange(
+    //   'menuData',
+    //   update(menuData, {
+    //     [index]: { [name]: { $set: value } },
+    //   })
+    // );
   };
 
   const handleDelete = id => {
@@ -57,27 +64,26 @@ export default function SortableMenu ({ onChange, menuId }) {
     onChange('menuData', remove(menuData, index));
   };
 
-  const handleClickAdd = () => {
-    onChange(
-      'menuData',
-      add(menuData, {
-        id: nanoid(8),
-        name: '',
-        menu_uuid: currentMenu,
-      })
-    );
-  };
+  const handleClickAdd = useCallback((data) => (e) => {
+    e.preventDefault()
+    console.log('aaaa', data)
+    setSortlyData(add(data, {name: ''}))
+  },[]);
 
+  const handleSortly = (newItems) => {
+    console.log('NEW ITEM::' , newItems)
+    setMenuData(newItems);
+  }
+  if (menuDataLoading) return <p>Loading...</p>
+  if (menuDataError) return <p>Error!</p>
   return (
     <>
       <div className="row">
         <Sortly
-          // items={menuData && menuData.items}
-          items={exampleMenu}
-          id={menuId}
+          items={sortlyData || []}
           name={menuData && menuData.title}
           // onChange={setSortly}
-          onChange={values => onChange('menuData', values)}
+          onChange={handleSortly}
         >
           {props => (
             <SortableTreeItem {...{ handleDelete, handleChangeRow }} {...props} />
@@ -85,7 +91,7 @@ export default function SortableMenu ({ onChange, menuId }) {
         </Sortly>
       </div>
       <div className="row">
-        <button kind="secondary" onClick={handleClickAdd}>
+        <button kind="secondary" onClick={handleClickAdd(sortlyData)}>
           <FormattedMessage id={'menu-editor.MenuEditor.add'} />
         </button>
       </div>
