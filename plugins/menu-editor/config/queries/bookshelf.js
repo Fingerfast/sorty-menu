679e15d28a5fe192dc0dd5ecc6821de3a3e3bbcb
Martin Novák <martin.novak@cleverlance.com>
2020-01-22 14:48:45 +0100

    fix: put request

--- plugins/menu-editor/config/queries/bookshelf.js
+++ plugins/menu-editor/config/queries/bookshelf.js
@@ -6,15 +6,26 @@ module.exports = model => ({
   putSourceMenu: async (menu_items) => {
     const Bookshelf = new bookshelf(strapi.connections.default);
     //TODO catch errors
+    //return await Bookshelf.knex.transaction(async trx => {
+    //  try {
+    //    await Bookshelf.knex('menu-editor_source_menu')
+    //      .transacting(trx)
+    //      .del();
+
+    //    await Bookshelf.knex('menu-editor_source_menu')
+    //      .transacting(trx)
+    //      .insert(menu_items);
+
+    //    await trx.commit;
+    //  } catch (error) {
+    //    await trx.rollback;
+    //  }
+    //});
     return await Bookshelf.knex.transaction(async trx => {
       try {
         await Bookshelf.knex('menu-editor_source_menu')
           .transacting(trx)
-          .del();
-
-        await Bookshelf.knex('menu-editor_source_menu')
-          .transacting(trx)
-          .insert(menu_items);
+          .update(menu_items);
 
         await trx.commit;
       } catch (error) {
