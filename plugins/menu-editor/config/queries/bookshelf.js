3e2c2fa61f6eec23c7ead527c04f73301046f55f
Martin Novák <martin.novak@cleverlance.com>
2020-01-22 13:49:15 +0100

    refactor: pages model into source_menus

--- plugins/menu-editor/config/queries/bookshelf.js
+++ plugins/menu-editor/config/queries/bookshelf.js
@@ -1,25 +1,25 @@
 const bookshelf = require('bookshelf');
 
 module.exports = model => ({
-  findOne: async ({uuid}) => await model.forge({uuid}).fetch(),
+  findOne: async ({id}) => await model.forge({id}).fetch(),
   find: async () => await model.fetchAll(),
-  putSourceMenu: async (menu_items) => {
-    const Bookshelf = new bookshelf(strapi.connections.default);
-    //TODO catch errors
-    return await Bookshelf.knex.transaction(async trx => {
-      try {
-        await Bookshelf.knex('menu-editor_source_menu')
-          .transacting(trx)
-          .del();
+  //postSourceMenu: async (menu_items) => {
+  //  const Bookshelf = new bookshelf(strapi.connections.default);
+  //  //TODO catch errors
+  //  return await Bookshelf.knex.transaction(async trx => {
+  //    try {
+  //      await Bookshelf.knex('menu-editor_source_menu')
+  //        .transacting(trx)
+  //        .del();
 
-        await Bookshelf.knex('menu-editor_source_menu')
-          .transacting(trx)
-          .insert(menu_items);
+  //      await Bookshelf.knex('menu-editor_source_menu')
+  //        .transacting(trx)
+  //        .insert(menu_items);
 
-        await trx.commit;
-      } catch (error) {
-        await trx.rollback;
-      }
-    });
-  },
+  //      await trx.commit;
+  //    } catch (error) {
+  //      await trx.rollback;
+  //    }
+  //  });
+  //},
 });
