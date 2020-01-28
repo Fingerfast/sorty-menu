8b63decadf062e4aef83ee924a10a5e9d6d7e6da
Martin Novák <martin.novak@cleverlance.com>
2020-01-28 18:23:20 +0100

    remove source_menu table and move context to Pages

--- plugins/menu-editor/controllers/Source_menu.js
+++ plugins/menu-editor/controllers/Source_menu.js
@@ -1,82 +1,16 @@
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 const Bookshelf = require('bookshelf');
 
 module.exports = {
-  /**
-   * Default action.
-   *
-   * @return {Object}
-   */
   index: async ctx => {
-    // Send 200 `ok`
-    ctx.send({
-      message: 'ok',
-    });
-  },
-  findOne: async ctx => {
-    const { id } = ctx.params
-    const Menu = strapi.plugins['menu-editor'].models.source_menu;
-
-    try {
-      const sourceMenu = await strapi.plugins['menu-editor'].config
-        .queries(Menu)
-        .findOne({id});
-      
-      ctx.body = sourceMenu;
-    } catch (error) {
-      console.log('getMenusList error:', error);
-      ctx.send(
-        {
-          message: 'error',
-          error,
-        },
-        400
-      );
-    }
   },
   find: async ctx => {
-    const Menu = strapi.plugins['menu-editor'].models.source_menu;
-
-    try {
-      const sourceMenu = await strapi.plugins['menu-editor'].config
-        .queries(Menu)
-        .find();
-
-      ctx.body = sourceMenu.serialize()
-    } catch (error) {
-      console.log('getMenusList error:', error);
-      ctx.send(
-        {
-          message: 'error',
-          error,
-        },
-        400
-      );
-    }
+    ctx.body = await strapi.controllers.pages.find(ctx)
   },
-  putSourceMenu: async ctx => {
-    const menu_items = ctx.request.body
-    const SourceMenu = strapi.plugins['menu-editor'].models.source_menu
-
-    try {
-      await strapi.plugins['menu-editor'].config
-        .queries(SourceMenu)
-        .putSourceMenu(menu_items)
-
-      ctx.send(
-        {
-          message: 'OK',
-        },
-        201
-      );
-    } catch (error) {
-      console.log('putMenuById error:', JSON.stringify(error));
-      ctx.send(
-        {
-          message: 'error',
-          error,
-        },
-        400
-      );
-    }
+  findOne: async ctx => {
+    ctx.body = await strapi.controllers.pages.findOne(ctx)
+  },
+  editMany: async ctx => {
+    ctx.body = await strapi.controllers.pages.updateMany(ctx)
   },
 };
